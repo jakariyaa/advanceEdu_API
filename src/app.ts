ff3bb7371526ec27869592ddc71308c2932f9737
jakariyaa <jakariya3460@gmail.com>
2026-01-16 00:49:00 +0600

    refactor: optimize imports in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,40 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { pinoHttp } from 'pino-http';
 import { logger } from './common/lib/logger';
 import { errorHandler } from './common/middleware/error-handler';
+import { csrfProtection } from './common/middleware/csrf.middleware';
 import { limiter } from './common/middleware/rate-limit';
 import routes from './routes';
 import webhookRoutes from './modules/webhooks/webhook.routes';
 
+import { corsOptions } from './config/cors';
+import { helmetOptions } from './config/security';
+
 const app = express();
 
-app.use(helmet());
-app.use(cors());
+app.use(helmet(helmetOptions));
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(limiter);
 
-app.use(pinoHttp({ logger }));
+app.use(pinoHttp({
+    logger,
+    autoLogging: true,
+    quietReqLogger: false,
+}));
 
 app.use('/api/webhooks', express.raw({ type: 'application/json' }), webhookRoutes);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes);
+app.use('/api', csrfProtection, routes);
 
 app.use(errorHandler);
 
 export default app;
+
+// Optimized imports
