96d5270e83365e70f6a9da0b3d25cb405f1de272
jakariyaa <jakariya3460@gmail.com>
2026-01-18 01:03:15 +0600

    refactor: extract constants in payment service

--- src/common/lib/stripe.ts
+++ src/common/lib/stripe.ts
@@ -1,5 +1,8 @@
 import Stripe from 'stripe';
+import { env } from './env';
 
-export const stripe = new Stripe(process.env['STRIPE_SECRET_KEY'] || 'sk_test_dummy', {
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
     apiVersion: '2025-12-15.clover',
 });
+
+export const STRIPE_API_VERSION = '2023-10-16';
