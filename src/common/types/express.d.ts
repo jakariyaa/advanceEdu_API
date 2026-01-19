08abf128f7593c80fe7f649c7a5b3d666473c205
jakariyaa <jakariya3460@gmail.com>
2026-01-19 23:38:36 +0600

    refactor: simplify error response type

--- src/common/types/express.d.ts
+++ src/common/types/express.d.ts
@@ -1,9 +1,17 @@
-import { User } from '@prisma/client';
+import { Role } from '../../generated/prisma/client';
 
 declare global {
     namespace Express {
         interface Request {
-            user?: Partial<User> & { userId: number };
+            user?: {
+                userId: number;
+                email: string;
+                name: string | null;
+                role: Role;
+                stripeId: string | null;
+            };
         }
     }
 }
+
+// Standardized error response interface
